e674a41dddcfd4c5cb898f14891fe7fe4ee57bd8
1103gracey <graceychapadia@gmail,com>
2021-11-21 20:20:12 -0500

    Second Commit

--- Gracey_Chapadia_8739519_ACH5.py
+++ Gracey_Chapadia_8739519_ACH5.py
@@ -1,18 +1,18 @@
 #This program will demonstrate the calculating total number of counts in givrn string.
 #This program is done by Gracey Chapadia - 8739519
 
-
+#define input string
 inputString = 'Lorem ipsum doflor szit ametß, consqecytetu$r adipibsching elit, sed do eiusmod tewmporα incid6kgidunt ut labq8ore et café gracey chapadia 8739519 dolore_ magna aliqua. Purus! sit{|}~ \t\n\r\x0b\x0c amet volutpat cons&jequat mau7ris. Penaxtibus et'
-
+#define logic for count numeric value
 numberValue = sum(count.isdigit() for count in inputString)
-
+#define logic for count alphanumeric value
 alphanumbericValue = sum(count.isalnum() for count in inputString)
-
+#define logic for count spaces
 spaceValue = sum(count.isspace() for count in inputString)
-
+#define logic for count non aplpha numeric value
 nonAlphanumbericValue = len(inputString) - numberValue -alphanumbericValue - spaceValue
 
-
+#providing appropriate output by printing messages
 print("Found " + str(inputString.count('g'))+ " matches of letter G.")
 print("Found " + str(numberValue) + " matches of digits.")
 print("Found " + str(alphanumbericValue) + " matches of AlphaNumeric characters.")
