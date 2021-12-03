b74781838ddc05586f44bd7b034bcb967f8d5c08
1103gracey <graceychapadia@gmail,com>
2021-12-03 08:24:00 -0500

    Final Commit

--- Gracey_Chapadia_8739519_Test3_Q1.py
+++ Gracey_Chapadia_8739519_Test3_Q1.py
@@ -3,8 +3,11 @@ from datetime import datetime
 #This program will demonstrate the date and time.
 #This program is done by Gracey Chapadia - 8739519
 
+#making variable which fetch the current date and time
 currentDateTime = datetime.now()
 
+#storing result of currentDateTime variable in to the new string and format the result using strftime
 dateAndTime = currentDateTime.strftime("%m/%d/%Y %H:%M:%S")
 
+#giving prompt message to the user
 print("The current date and time is: ", str(dateAndTime))
