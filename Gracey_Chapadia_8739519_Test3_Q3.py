8c251b0794349b53cbfe06c848b7404d6b79b408
1103gracey <graceychapadia@gmail,com>
2021-12-03 09:27:12 -0500

    Final Commit

--- Gracey_Chapadia_8739519_Test3_Q3.py
+++ Gracey_Chapadia_8739519_Test3_Q3.py
@@ -2,10 +2,14 @@ n = int(input("Enter the ending number: "))
 sum = 0
 m = 0
 
+#write 1 instead of 2
 for num in range(1, n + 1, 1):
     sum = sum + num
 
 print("The sum of the first", n, "numbers is: ", sum)
 
+#remove n+1 and add only n to the code to find the average
 average = sum / n
+
+#change n instead of m, there is no need of m in the code
 print("The average of the first", n, "numbers is: ", average)
